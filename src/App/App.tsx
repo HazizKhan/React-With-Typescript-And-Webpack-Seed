911bf9dfcf75b8c11eb2ff29a3bd879d0bd0f344
Haziz Khan <hazizkhan1999@gmail.com>
2017-06-14 11:08:33 +0500

    React seed

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export class App extends React.Component<{}, any> {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <h1>
+                Hello World using React!!
+            </h1>
+        )
+    }
+}
